97d19d9b299c52760317a911090716ac2e567526
Richard <richard.guilland@etu.univ-lyon1.fr>
2022-05-12 14:30:22 +0200

    Vérification lecture fichier clean

--- src/main/java/com/risa/functionality/load/LoadGraphFromCSV.java
+++ src/main/java/com/risa/functionality/load/LoadGraphFromCSV.java
@@ -3,27 +3,33 @@ package com.risa.functionality.load;
 import com.risa.graph.*;
 
 import java.io.*;
-import java.util.ArrayList;
 
 public class LoadGraphFromCSV {
 
+    /**
+     *
+     * @param path chemin absolu vers le fichier
+     * @return
+     */
      public Graph load(String path)  {
+         Graph graph = new Graph();
 
-         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path) )) {
+         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
              String line;
+
              while ((line = bufferedReader.readLine()) != null) {
                  System.out.println(line);
+
+                 /*
+                 String[] splitted = line.split(";;");
+                 System.out.println(Arrays.toString(splitted));
+                 */
              }
          } catch (IOException e) {
-           e.printStackTrace();
+             graph = null;
+             System.out.println("Erreur lors du chargement du graphe.");
          }
 
-         return null;
+         return graph;
      }
-
-
-
 }
-
-
-
--- src/main/java/com/risa/functionality/load/test.java
+++ src/main/java/com/risa/functionality/load/test.java
@@ -1,11 +1,10 @@
 package com.risa.functionality.load;
 
 public class test {
-
     public static void main(String[] args) {
         System.out.println("DEBUT ----------------------------------------");
         LoadGraphFromCSV t = new LoadGraphFromCSV();
-        t.load("C:\\Users\\etulyon1\\Documents\\SAE-GraphMap\\src\\main\\java\\com\\risa\\functionality\\load\\test.csv");
+        t.load(System.getProperty("user.dir") + "\\src\\main\\java\\com\\risa\\functionality\\load\\test.csv");
         System.out.println("FIN ----------------------------------------");
     }
 }

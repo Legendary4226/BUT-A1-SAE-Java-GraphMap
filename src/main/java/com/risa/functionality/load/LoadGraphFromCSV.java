7223babd282199cbd438e826d6329a02d3120272
Sadiya <sadiya.niang@etu.univ-lyon1.fr>
2022-05-24 10:49:47 +0200

    Work: Continue LoadFromFile

--- src/main/java/com/risa/functionality/load/LoadGraphFromCSV.java
+++ src/main/java/com/risa/functionality/load/LoadGraphFromCSV.java
@@ -3,6 +3,8 @@ package com.risa.functionality.load;
 import com.risa.graph.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class LoadGraphFromCSV {
@@ -14,6 +16,7 @@ public class LoadGraphFromCSV {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             while ((line = bufferedReader.readLine()) != null) {
                 split(line);
+                System.out.println(line);
             }
         } catch (IOException e) {
             graph = null;
@@ -23,21 +26,39 @@ public class LoadGraphFromCSV {
         return graph;
     }
 
-    public void split (String line){
+    public void split (String line) {
         String[] splitted = line.split( ";;");
-        String[] noeudSource = splitted[0].split("&");
-        graph.ajouterNoeud(noeudSource[0],TypeLieu.valueOf(noeudSource[1]) );
-        stockageStructure.put(noeudSource[0], new HashMap<>());
-        split2(splitted[1], noeudSource[0]);
-    }
+        String nomDUNoeud = splitted[0].split("&")[0];
+        System.out.println(nomDUNoeud);
+        String typelieu = splitted[0].split("&")[1];
+        System.out.println(typelieu);
+        graph.ajouterNoeud(nomDUNoeud, valueOfTypeLieu(typelieu));
+        stockageStructure.put = (nomDUNoeud  new HashMap<>());
 
+        String reste = String.valueOf(splitted[1].split(";;"));
+        System.out.println(reste);
+
+       // stockageStructure.put(reste, new HashMap<>());
+
+
+
+
+       /*
+        String[] noeudSource = splitted[0].split(":");
+        graph.ajouterNoeud(noeudSource[0],TypeLieu.valueOf(noeudSource[1]));
+        stockageStructure.put(noeudSource[0], new HashMap<>());
+       split2(splitted[1], noeudSource[0]);*/
+    }
+/*
     public void split2(String arete, String noeudPrincipal){
         String[] r = arete.split (";");
         for (String noeudDestinationPlusRoute: r){
             String [] t = noeudDestinationPlusRoute.split ("&");
             stockageStructure.get(noeudPrincipal).put(t[0], t[1]);
         }
-    }
+    } */
+
+
    /*  public void split3(String reste){
          String[] r = reste.split ("");
 
@@ -87,4 +108,32 @@ public class LoadGraphFromCSV {
         return line;
     }
 */
+
+    private TypeLieu valueOfTypeLieu(String typeLieu) {
+        if (typeLieu.equals("V")) {
+            return TypeLieu.VILLE;
+        }
+        if (typeLieu.equals("C")) {
+            return TypeLieu.CENTRE_LOISIR;
+        }
+        if (typeLieu.equals("R")) {
+            return TypeLieu.RESTAURANT;
+        }
+
+        return null;
+    }
+
+    private TypeRoute valueOfTypeRoute(String typeRoute) {
+        if (typeRoute.equals("D")) {
+            return TypeRoute.DEPARTEMENTALE;
+        }
+        if (typeRoute.equals("A")) {
+            return TypeRoute.AUTOROUTE;
+        }
+        if (typeRoute.equals("N")) {
+            return TypeRoute.NATIONALE;
+        }
+
+        return null;
+    }
 }
--- src/main/java/com/risa/functionality/load/test.java
+++ src/main/java/com/risa/functionality/load/test.java
@@ -7,7 +7,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("DEBUT ----------------------------------------");
         LoadGraphFromCSV t = new LoadGraphFromCSV();
-        t.load(new File("C:\\Users\\etulyon1\\Documents\\SAE-GraphMap\\src\\main\\java\\com\\risa\\functionality\\load\\test.csv"));
+        t.load(new File(System.getProperty("user.dir") + "\\src\\main\\java\\com\\risa\\functionality\\load\\test.csv"));
         System.out.println("FIN ----------------------------------------");
     }
 }

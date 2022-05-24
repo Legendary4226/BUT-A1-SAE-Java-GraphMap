7714c936bca4809df235cf0ce45d6e9683c17545
Sadiya <sadiya.niang@etu.univ-lyon1.fr>
2022-05-24 12:00:14 +0200

    Work: Clean Working after fix

--- src/main/java/com/risa/functionality/load/test.java
+++ src/main/java/com/risa/functionality/load/test.java
@@ -1,12 +1,14 @@
 package com.risa.functionality.load;
 
+import com.risa.graph.Graph;
+
 import java.io.File;
 
 public class test {
 
     public static void main(String[] args) {
         System.out.println("DEBUT ----------------------------------------");
-        LoadGraphFromCSV t = new LoadGraphFromCSV();
+        LoadGraphFromCSV t = new LoadGraphFromCSV(new Graph());
         t.load(new File(System.getProperty("user.dir") + "\\src\\main\\java\\com\\risa\\functionality\\load\\test.csv"));
         System.out.println("FIN ----------------------------------------");
     }
--- src/main/java/com/risa/graphicinterface/TestGraph.java
+++ src/main/java/com/risa/graphicinterface/TestGraph.java
@@ -3,7 +3,6 @@ package com.risa.graphicinterface;
 public class TestGraph {
 
     public static void main(String[] args) {
-        com.risa.graph.Graph graphSAE = new com.risa.graph.Graph();
-        new MainWindow(graphSAE);
+        new MainWindow();
     }
 }
